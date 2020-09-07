35bb2356514b7f953cdbecdb352e18658dd77693
Mathias Madsen <MathiasMadsen@Mathiass-MacBook-Pro-2.local>
2020-09-07 11:24:07 +0200

    Initial commit

---
+++ src/Hello.java
@@ -0,0 +1,8 @@
+public class Hello {
+    // for og få den her skriv pvsm
+    public static void main(String[] args) {
+        // for og få den her skriv sout
+        System.out.println("Hello World");
+        System.out.println("Hello");
+    }
+}
